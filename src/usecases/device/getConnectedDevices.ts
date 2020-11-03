e6200e5e5503a6639f9b8ef28aad0639878fbad9
maged dev <maged.dev@gmail.com>
2020-11-03 04:46:30 +0200

    .

--- src/usecases/device/getConnectedDevices.ts
+++ src/usecases/device/getConnectedDevices.ts
@@ -4,8 +4,11 @@ import { IDeviceRepo, IUserRepo } from '@Repos';
 import { IDevice } from '@Entities';
 import { IHttpRequest } from '@Usecases/interfaces/HttpRequest';
 
-// query store for connected-devices that belongs to a user
+// TODO: should the usecase know about the HTTP request ?
+// it should only contain business logic
+// and be agnostic of the caller/environment, so it can be called from cli or web or any client
 
+/* query store for connected-devices that belongs to a user */
 export default async function execute(
   request: IHttpRequest
 ): Promise<IDevice[] | []> {
